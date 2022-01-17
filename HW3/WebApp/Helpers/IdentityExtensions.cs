2d7745cfb795daa4d8ff3325c4eb090b58853920
Giorgi Zeikidze <gizeik@ttu.ee>
2022-01-17 17:10:14 +0200

    Empty WebApplication

---
+++ HW3/WebApp/Helpers/IdentityExtensions.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+using System.Security.Claims;
+
+namespace WebApp.Helpers
+{
+    public static class IdentityExtensions
+    {
+        // extension method
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            var idClaim = user.Claims.FirstOrDefault(c =>
+                c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
+            return idClaim?.Value;
+        }
+        
+    }
+}
